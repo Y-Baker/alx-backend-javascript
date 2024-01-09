379723e000ecab28f2b63defbe21a91591eb5c0c
Yousef <yuossefbakier@gmail.com>
2024-01-09 13:25:36 +0200

    Add Task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+import ClassRoom from './0-classroom';
+
+function initializeRooms() {
+  return [19, 20, 34].map((size) => new ClassRoom(size));
+}
+
+export default initializeRooms;
