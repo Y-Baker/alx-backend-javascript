8031a1a5b55ffe31301e1c81108251297e99bfe0
Yousef <yuossefbakier@gmail.com>
2024-01-09 15:50:23 +0200

    ignore lint error

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable max-classes-per-file */
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -35,7 +37,6 @@ export class StudentHolberton {
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
