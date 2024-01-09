d0486f1335f4a0301af44a786326e51b1c23a4e5
Yousef <yuossefbakier@gmail.com>
2024-01-09 13:20:04 +0200

    Add Task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
