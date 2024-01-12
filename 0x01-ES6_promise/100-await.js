6893b78fc84c59577331d7ee73fbd18002c00695
Yousef <yuossefbakier@gmail.com>
2024-01-12 19:09:49 +0200

    Finish Task Promise

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { createUser, uploadPhoto } from "./utils";
+
+async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+
+    return {photo, user};
+  } catch (err) {
+    return ({ photo: null, user: null });
+  }
+}
+
+export default asyncUploadUser;
