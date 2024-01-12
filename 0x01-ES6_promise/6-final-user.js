6a8f451c944af56f53f5370623415dd179d8cf71
Yousef <yuossefbakier@gmail.com>
2024-01-12 17:48:48 +0200

    Add Task 6 Promise

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(fileName, lastName),
+    uploadPhoto(fileName),
+  ]).then((data) => {
+    const result = [];
+    for (const one of data) {
+      result.push({
+        status: one.status,
+        value: one.status === "fulfilled" ? one.value : one.reason,
+      });
+    }
+    return result;
+  });
+}
+
+export default handleProfileSignup;
